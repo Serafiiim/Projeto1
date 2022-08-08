af7f5add103bf198397216820141ca9e0ef494e3
Serafiiim <Jpedroserafim@hotmail.com>
2022-08-08 17:10:23 -0300

    Urls and Viwes no app

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('HOME 2')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+# Create your views here.
