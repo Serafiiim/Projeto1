decdf463ef38a6f8358f2e54244cc7ec12358984
Serafiiim <Jpedroserafim@hotmail.com>
2022-08-08 19:00:45 -0300

    Font-awesome

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
+    return render(request, 'recipes/home.html', context={
+        'name': 'João Serafim',
+    })
 # Create your views here.
